2c5550b5bf1a8ae4c2cc9d85241d9a94334b705f
Diego Jimenez <66481204+Diego-Jimenez423@users.noreply.github.com>
2020-09-11 01:06:54 -0600

    enu desplegable completado

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,17 @@ btnMenu.addEventListener('click', function(){
     'use strict';
     menu.classList.toggle('mostrar');
 });
+
+var botonlis4 = document.getElementById("lisBoton4");
+var boton4 = document.getElementById("boton4");
+
+function desplegarBoton4(){
+    botonlis4.classList.add("mostrar");
+}
+
+function ocultarBoton4(){
+botonlis4.classList.remove("mostrar");    
+}
+
+boton4.addEventListener("mouseover",desplegarBoton4,true);
+boton4.addEventListener("mouseout",ocultarBoton4,true);
